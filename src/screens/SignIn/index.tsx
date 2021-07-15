5c4c65b96302aded1422d24f39a6368b222b7b10
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-15 15:30:20 -0300

    :recycle: change text title

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -22,13 +22,13 @@ export function SignIn() {
 
       <View style={styles.text_content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
