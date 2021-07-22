f1f909abee211bc5d89ba1f91f1c3608b7af5b99
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-22 11:30:48 -0300

    :recycle: change avatar size

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -2,16 +2,16 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    width: 49,
-    height: 49,
+    width: 48,
+    height: 48,
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
     marginRight: 22
   },
   avatar: {
-    width: 46,
-    height: 46,
+    width: 47,
+    height: 47,
     borderRadius: 8,
   }
 })
